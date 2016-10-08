c9a964a6e3b6cd2793aca137f1ef9cba13fa7f8a
Andre <andre.jemison@gmail.com>
2016-10-08 12:54:59 -0700

    changed security features for deployment

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1*a!b)w1lkbxaz@7!@2tl+znme$5&q7^1j-!)-r@wd5j-c_e+4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*.elasticbeanstalk.com']
 
 
 # Application definition
