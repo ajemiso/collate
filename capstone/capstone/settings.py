bcb62b63d4a83e42899ed19346d37012afd9a605
Andre <andre.jemison@gmail.com>
2016-10-07 20:28:45 -0700

    addt'l deployment config

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -156,8 +156,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/templates/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "..", "www", "submittals", "static"),
-    ]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "..", "www", "submittals", "static"),
+#     ]
 
